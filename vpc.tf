38be6d43d9d287a9920a5b8f9f099e29affa54f8
joshiap21 <apj.gca@gmail.com>
2021-11-07 15:28:30 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,16 @@
+module "network" {
+  network_name = "gaurav-network"
+  project_id = var.project_id
+  source  = "app.terraform.io/apj-driven-workflow/network/google"
+  version = "3.4.0"
+  subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+  # insert required variables here
+}
+
+
